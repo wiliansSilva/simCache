2a133f81425f9270de33aaaad95c296e0629ebb8
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-05-22 21:47:28 -0300

    Setando conjuntos

--- src/Conjuntos.java
+++ src/Conjuntos.java
@@ -1,16 +1,27 @@
+import java.util.*;
 public class Conjuntos {
 
-    private String[] entradas = new String[100];
-    private String tag;
-    private int i = 0;
+   
+    ArrayDeque<String> conjunto; 
+    int tamanho;
 
-    public Conjuntos(String tag){ //Passa o tamanho da entrada
-        this.tag = tag;
+    public Conjuntos(int tamanho){
+        this.tamanho = tamanho;
+    }
+    
+    int confereTag(String tag) {
+    	//Vai percorrer todo o conjunto e conferir se possui a tag
+    	if(conjunto.contains(tag)) {
+    		//Caso a tag esteja presente atualizar que o dado foi acessado
+    		//Isso permite que consigamos analisar o ultimo dado acessado 
+    		//Possibilitando a verificação pra implementaçao de substituição LRU
+    		atualizaConjunto(tag){//A ser implementado
+    			return 1
+    		}
+    	//Caso a tag não esteja no conjunto
+    	} else {
+			insereNoConjunto(tag)//A ser implementado
+		}
     }
 
-    public int aloca(){
-        entradas[i] = tag;
-        i++;
-        return 0;
-    }
 }
